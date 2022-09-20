904c067c7264acc7b5d27b68bd93dc5c8a13560e
Hikaru <34558365+Hikali-47041@users.noreply.github.com>
2022-09-20 15:30:44 +0900

    Update button.vb

--- CopyCellValuetoClipBoard/button.vb
+++ CopyCellValuetoClipBoard/button.vb
@@ -1,1 +1,5 @@
-
+Sub Button1_Click()
+    Const ROW As Integer = 2
+    Call CopyCellPlaceValueToClipboard("C" & ROW)
+    Application.StatusBar = "Copied: " & Range("A" & ROW) & " " & Range("B" & ROW)
+End Sub
