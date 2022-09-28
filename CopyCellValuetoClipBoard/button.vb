c52a1de3942123ce30f814069b03907ad1f0dc43
Hikaru <34558365+Hikali-47041@users.noreply.github.com>
2022-09-28 10:46:50 +0900

    add: comment

--- CopyCellValuetoClipBoard/button.vb
+++ CopyCellValuetoClipBoard/button.vb
@@ -1,5 +1,8 @@
-Sub Button1_Click()
-    Const ROW As Integer = 2
-    Call CopyCellPlaceValueToClipboard("C" & ROW)
-    Application.StatusBar = "Copied: " & Range("A" & ROW) & " " & Range("B" & ROW)
+Sub button1_Click()
+    ' URLがある行の値(数字)を入力(ここの値のみを変えてください)
+    Const ROW As String = "2"
+    ' URLが記載されているC3のセルの中身をクリップボードにコピーさせる(copyCellValueToClipboard)関数を呼び出し
+    Call copyCellValueToClipboard("C" & ROW)
+    ' ExcelのステータスバーにURLをコピーしたことを表示させる
+    Application.StatusBar = "[" & Range("A" & ROW).Value & " " & Range("B" & ROW).Value & "] のURLをコピーしました!"
 End Sub
