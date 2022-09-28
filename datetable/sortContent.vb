dd36d6529948ffc5af9764257e2ac076eaec6af2
Hikaru <34558365+Hikali-47041@users.noreply.github.com>
2022-09-28 10:51:02 +0900

    mod: Sort2 -> Sort

--- datetable/sortContent.vb
+++ datetable/sortContent.vb
@@ -1,20 +1,21 @@
 Sub sortContent()
-    Const SHEETNAME As String = "Sheet1"
+    ' シート名は適切な名前に変更してください
+    Const SHEETNAME As String = "Sheet1" 
     Const FLAGHEAD As String = "C1"
     Const DATEHEAD As String = "A1"
     Const HIDARIUE As String = "A1"
-    Worksheets(SHEETNAME).Sort.SortFields.Clear
-    Worksheets(SHEETNAME).Sort.SortFields.Add2 _
+    Worksheets(SHEETNAME).sort.SortFields.Clear
+    Worksheets(SHEETNAME).sort.SortFields.Add _
         Key:=Range(FLAGHEAD), _
         SortOn:=xlSortOnValues, _
         Order:=xlDescending, _
         DataOption:=xlSortNormal
-    Worksheets(SHEETNAME).Sort.SortFields.Add2 _
+    Worksheets(SHEETNAME).sort.SortFields.Add _
         Key:=Range(DATEHEAD), _
         SortOn:=xlSortOnValues, _
         Order:=xlDescending, _
         DataOption:=xlSortNormal
-    With Worksheets(SHEETNAME).Sort
+    With Worksheets(SHEETNAME).sort
         .SetRange Range(HIDARIUE, Range(HIDARIUE).SpecialCells(xlCellTypeLastCell))
         .Header = xlYes
         .MatchCase = False
