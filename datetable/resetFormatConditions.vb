4ef7def8a3d0570e0040279874caee9be5a3df95
Hikaru <34558365+Hikali-47041@users.noreply.github.com>
2022-09-28 10:48:36 +0900

    mod: remove some formatconditions

--- datetable/resetFormatConditions.vb
+++ datetable/resetFormatConditions.vb
@@ -1,43 +1,34 @@
 ' XlRgbColor is BGR decimal
 Sub resetFormatConditions()
-    Cells.FormatConditions.Delete
-    Const SHEETNAME As String = "Sheet1"
+    Application.StatusBar = "条件付き書式を再設定中"
+    Cells.formatconditions.Delete
+    Const SHEETNAME As String = "申し送り事項"
     Const TARGETCOLUMNS As String = "A:C"
     Dim fc As FormatCondition
     With Worksheets(SHEETNAME).Columns(TARGETCOLUMNS)
         ' important highlight #1
-        Set fc = .FormatConditions.Add (Type:=xlExpression, _
-            Formula1:= "=$C1=1") 
+        Set fc = .formatconditions.Add(Type:=xlExpression, _
+            Formula1:="=$C1=""重要""")
         With fc.Interior
             .PatternColorIndex = xlAutomatic
             .Color = 16048383 ' #ffe0f4
             .TintAndShade = 0
         End With
         fc.StopIfTrue = False
-        
-        ' important highlight #2
-        Set fc = .FormatConditions.Add (Type:=xlExpression, _
-            Formula1:= "=$C1=2") 
-        With fc.Interior
-            .PatternColorIndex = xlAutomatic
-            .Color = 15385599 ' #ffc3ea
-            .TintAndShade = 0
-        End With
-        fc.StopIfTrue = False
 
         ' odd line white
-        Set fc = .FormatConditions.Add (Type:=xlExpression, _
-                Formula1:= "=AND(ISODD(ROW($A1)), NOT($A1=""""))")
+        Set fc = .formatconditions.Add(Type:=xlExpression, _
+                Formula1:="=AND(ISODD(ROW($A1)), NOT($A1=""""))")
             With fc.Interior
                 .PatternColorIndex = xlAutomatic
-                .ThemeColor = xlThemeColorDark1
+                .Color = 16579836 ' #fcfcfc
                 .TintAndShade = 0
             End With
             fc.StopIfTrue = False
 
         ' even line light gray
-        Set fc = .FormatConditions.Add (Type:=xlExpression, _
-                Formula1:= "=AND(ISEVEN(ROW($A1)), NOT($A1=""""))") 
+        Set fc = .formatconditions.Add(Type:=xlExpression, _
+                Formula1:="=AND(ISEVEN(ROW($A1)), NOT($A1=""""))")
             With fc.Interior
                 .PatternColorIndex = xlAutomatic
                 .Color = 15855855 ' #eff0f1 -> 15855855
@@ -46,7 +37,7 @@ Sub resetFormatConditions()
             fc.StopIfTrue = False
 
         ' underline day diff
-        Set fc = .FormatConditions.Add (Type:=xlExpression, _
+        Set fc = .formatconditions.Add(Type:=xlExpression, _
                 Formula1:="=$A1<>$A2")
             With fc.Borders(xlBottom)
                 .LineStyle = xlContinuous
@@ -55,4 +46,5 @@ Sub resetFormatConditions()
             End With
             fc.StopIfTrue = False
     End With
+    Application.StatusBar = False
 End Sub
